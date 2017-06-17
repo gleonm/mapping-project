427d0a3abe10bb267701d64ac7f6ed461aba4379
gerardoleon <leon.gerardo10@gmail.com>
2017-06-16 20:26:15 -0500

    Final

--- www/js/MappingApp.js
+++ www/js/MappingApp.js
@@ -56,82 +56,6 @@
    *******************/
 
   // WebSocket
-  var wsURL = 'ws://192.168.43.148:1881';
-  var connection;
-
-  function setupWebSocket () {
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-
-    connection = new WebSocket (wsURL);
-
-    connection.onopen    = onConnectionOpen;
-    connection.onerror   = onConnectionError;
-    connection.onmessage = onConnectionMessage;
-  }
-
-  function onConnectionOpen () {
-    console.log('Connected with server!');
-
-    var message = {
-      type : 'register',
-      data : 'output'
-    };
-    connection.send(JSON.stringify(message));
-  }
-
-  function onConnectionError () {
-    console.log('Error in connection :(!');
-  }
-
-  function onConnectionMessage (event) {
-    var payload = event.data;
-    var message = JSON.parse(payload);
-    console.log(message);
-
-    var type = message.type;
-    var data = message.data;
-
-    if (type === 'newInput') {
-      createNewMesh(data);
-    }
-    else if (type === 'angle') {
-      onAngleReceived(data.from, data.angle);
-    }
-    else if (type === 'disconnected') {
-      onClientDisconnected(data);
-    }
-  }
-
-  function createNewMesh (idConnection) {
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.z = -5;
-    mesh.position.x = (Math.random() * 10) - 5;
-    mesh.position.y = (Math.random() * 10) - 5;
-    scene.add(mesh);
-
-    meshes[idConnection] = mesh;
-  }
-
-  function onAngleReceived (from, angle) {
-    var mesh = meshes[from];
-    if (mesh) {
-      mesh.rotation.z = angle * (Math.PI / 180.0) * -1.0;
-    }
-  }
-
-  function onClientDisconnected (from) {
-    var mesh = meshes[from];
-    if (mesh) {
-      scene.remove(mesh);
-      delete meshes[data];
-    }
-  }
-
-  /*******************
-   * WebSocket logic *
-   *******************/
-
-  // WebSocket
   var wsURL = 'ws://localhost:1881';
   var connection;
 
@@ -188,9 +112,9 @@
     character.setTextMaterial(new THREE.MeshNormalMaterial());
 
     // Se posiciona
-    var angle = random(PI * 0.25, PI * 0.75);
-    character.object.position.x = Math.cos(angle) * 30;
-    character.object.position.z = -Math.sin(angle) * 30;
+    //var angle = random(PI * 0.25, PI * 0.75);
+    //character.object.position.x = Math.cos(angle) * 30;
+    //character.object.position.z = -Math.sin(angle) * 30;
 
     // Se agrega al mundo
     world.scene.add(character.object);
